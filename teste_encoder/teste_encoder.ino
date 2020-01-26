999e8ab87dbb5cddb4b577c7c0fa76a1ccf5348a
William Ribeiro <william-cribeiro@bct.ect.ufrn.br>
2020-01-26 20:22:33 -0300

    Encoder left and rigth created

--- teste_encoder/teste_encoder.ino
+++ teste_encoder/teste_encoder.ino
@@ -1,24 +1,29 @@
-    /*<Program to test the motors and encoder to project Alice>
+/*<Program to test the motors and encoder to project Alice>
     Copyright (C) <2020>  <William Ribeiro>
-
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.*/
 
-const byte Encoder_C1 = 2;
-const byte Encoder_C2 = 4;
-byte Encoder_C1Last;
-int duracao;
-boolean Direcao;
+const byte Encoder_C1_1 = 2;
+const byte Encoder_C2_1 = 4;
+byte Encoder_C1_1Last_ESQ;
+int duracao_ESQ;
+boolean Direcao_ESQ;
+
+
+const byte Encoder_C1_2 = 3;
+const byte Encoder_C2_2 = 8;
+byte Encoder_C1_1Last_DIR;
+int duracao_DIR;
+boolean Direcao_DIR;
+
 
 const int AIA = 9;  // (pwm) pino 9 conectado ao pino A-IA do Módulo
 const int AIB = 5;  // (pwm) pino 5 conectado ao pino A-IB do Módulo
@@ -38,9 +43,11 @@ void setup() {
 }
  
 void loop() {//O programa ficará em loop, girando um motor para um lado, depois para o outro e depois troca de motor e repete
-  forward();
-  Serial.print("Pulso: ");
-  Serial.println(duracao);
+  backward();
+  Serial.print("Pulso Direito: ");
+  Serial.println(duracao_DIR);
+  Serial.print("Pulso Esquerdo: ");
+  Serial.println(duracao_ESQ);
   delay(1000);
   
 }
@@ -80,27 +87,51 @@ void right()
 
 void EncoderInit()
 {
-  pinMode(Encoder_C2, INPUT);
-  attachInterrupt(0, calculapulso, CHANGE);
+  pinMode(Encoder_C2_1, INPUT);
+  pinMode(Encoder_C2_2, INPUT);
+
+  attachInterrupt(0, calculapulso_ESQ, CHANGE);
+  attachInterrupt(1, calculapulso_DIR, CHANGE);
 }
 
-void calculapulso()
+void calculapulso_ESQ()
 {
-  int Lstate = digitalRead(Encoder_C1);
-  if ((Encoder_C1Last == LOW) && Lstate == HIGH)
+  int Lstate = digitalRead(Encoder_C1_1);
+  if ((Encoder_C1_1Last_ESQ == LOW) && Lstate == HIGH)
   {
-    int val = digitalRead(Encoder_C2);
-    if (val == LOW && Direcao)
+    int val = digitalRead(Encoder_C2_1);
+    if (val == LOW && Direcao_ESQ)
     {
-      Direcao = false; //Reverse
+      Direcao_ESQ = false; //Reverse
     }
-    else if (val == HIGH && !Direcao)
+    else if (val == HIGH && !Direcao_ESQ)
     {
-      Direcao = true;  //Forward
+      Direcao_ESQ = true;  //Forward
     }
   }
-  Encoder_C1Last = Lstate;
+  Encoder_C1_1Last_ESQ = Lstate;
 
-  if (!Direcao)  duracao++;
-  else  duracao--;
+  if (!Direcao_ESQ)  duracao_ESQ++;
+  else  duracao_ESQ--;
+}
+
+void calculapulso_DIR()
+{
+  int Lstate = digitalRead(Encoder_C1_2);
+  if ((Encoder_C1_1Last_DIR == LOW) && Lstate == HIGH)
+  {
+    int val = digitalRead(Encoder_C2_2);
+    if (val == LOW && Direcao_DIR)
+    {
+      Direcao_DIR = false; //Reverse
+    }
+    else if (val == HIGH && !Direcao_DIR)
+    {
+      Direcao_DIR = true;  //Forward
+    }
+  }
+  Encoder_C1_1Last_DIR = Lstate;
+
+  if (!Direcao_DIR)  duracao_DIR++;
+  else  duracao_DIR--;
 }
